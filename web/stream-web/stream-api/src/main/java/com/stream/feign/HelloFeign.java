7e6d29e8e01c6bd61896ac2d55d02f243161007e
lihongwei1239@outlook.com <lihongwei1239@outlook.com>
2017-11-06 17:47:40 +0800

    dev

--- web/stream-web/stream-api/src/main/java/com/stream/feign/HelloFeign.java
+++ web/stream-web/stream-api/src/main/java/com/stream/feign/HelloFeign.java
@@ -1,12 +1,12 @@
 package com.stream.feign;
 
 import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient(name= "stream-order-service-web",fallbackFactory = HelloFeignHystrix.class)
 public interface HelloFeign {
 
-    @RequestMapping(value = "/hello")
-    String hello(@RequestParam(value = "name") String name);
+    @RequestMapping(value = "/hello/{name}")
+    String hello(@PathVariable("name") String name);
 }
